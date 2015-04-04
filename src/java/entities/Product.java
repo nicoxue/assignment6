7d69fb2272b21e634e9d25bf351979bc5f5679d3
xuejie87 <xuejie87@Maple-PC.mshome.net>
2015-04-04 15:58:29 -0400

    initial

---
+++ src/java/entities/Product.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/**
+ *
+ * @author xuejie87
+ */
+public class Product {
+    private int productId;
+    private String name;
+    private String description;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(int productId, String name, String description, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public Product(JsonObject json ){
+        productId = json.getInt("productId");
+        name = json.getString("name");
+        description = json.getString("description");
+        quantity = json.getInt("quantity");
+    }
+    
+    public JsonObject toJSON(){
+        return Json.createObjectBuilder()
+                .add("productId", productId)
+                .add("name", name)
+                .add("description",description)
+                .add("quantity",quantity)
+                .build();
+    }
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductid(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
